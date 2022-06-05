70e9c05a79858330b5815d4d48bf1f44fa1dca74
Vilja <i@vilja.me>
2022-06-05 18:06:22 +0000

    Update plugin params type.

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,12 +1,12 @@
 import { MZFMPlugin } from "@mzfm/common"
 import { EditChoices } from "./commands/EditChoices"
 
-const PARAMS = {}
+const DEFAULT_PARAMS = {}
 const COMMANDS = {
   EditChoices,
 }
-export const PLUGIN: MZFMPlugin<typeof PARAMS, typeof COMMANDS> = {
+export const PLUGIN: MZFMPlugin<typeof DEFAULT_PARAMS, typeof COMMANDS> = {
   name: "MZFM_Littles",
-  params: PARAMS,
+  default_params: DEFAULT_PARAMS,
   commands: COMMANDS,
 }
